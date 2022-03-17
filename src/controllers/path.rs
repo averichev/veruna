f1b9105b378cc9cf690cbf128515282474a3a552
aaverichev <Aleksandr.Averichev@hermesrussia.ru>
2022-03-17 10:38:59 +0300

    improvement: Various improvements

--- src/controllers/path.rs
+++ src/controllers/path.rs
@@ -1,14 +1,40 @@
-use actix_web::{HttpRequest, HttpResponse, web};
-use actix_web::error::InternalError;
+use actix_web::{Error, HttpRequest, HttpResponse};
+use actix_web::error::{InternalError};
 use actix_web::http::StatusCode;
-use sailfish::TemplateOnce;
 
 pub async fn path_test(
     req: HttpRequest
 )
-    -> actix_web::Result<HttpResponse>
+    -> actix_web::Result<HttpResponse, Error>
 {
+    let path = req
+        .path()
+        .to_string();
+    let nodes: Vec<String> = path
+        .split("/")
+        .map(|s| s.to_string())
+        .filter(|v| !v.is_empty())
+        .collect();
+    let len = nodes.len();
+    if len == 0 {
+        return Ok(HttpResponse::from_error(InternalError::new(
+            format!("Path not found {}", path),
+            StatusCode::NOT_FOUND,
+        )));
+    }
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(req.path().to_string()))
+        .body(path.to_string()))
+}
+
+
+pub async fn redirect_favicon()
+    -> actix_web::Result<HttpResponse, Error>
+{
+    let response =  HttpResponse::MovedPermanently()
+        .insert_header(("Location", "/static/favicon.ico"))
+        .finish();
+    Ok(
+       response
+    )
 }
