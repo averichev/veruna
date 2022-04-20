7c8c5fd54c25a418ab28d406989999506c6e0d68
averichev <averichev@yandex.ru>
2022-04-20 12:10:45 +0300

    improvement: db service

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,3 +1,15 @@
+use actix_web::error::InternalError;
+use actix_web::http::StatusCode;
+use sea_orm::DbErr;
+
 pub mod node_site_relation_service;
 pub mod site_service;
 pub mod component_service;
+pub mod db_service;
+
+pub fn internal_db_error(error: DbErr) -> InternalError<String> {
+    InternalError::new(
+        format!("DB error {}", error.to_string()),
+        StatusCode::INTERNAL_SERVER_ERROR,
+    )
+}
