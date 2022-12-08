84c28b322c44cd883e2d213cf751ad88e36b9397
averichev <averichev@yandex.ru>
2022-12-08 10:49:37 +0300

    improvement: Actix integration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use std::ops::Deref;
-use veruna_kernel::sites::{site_kit, SiteBuilderImpl, SiteReadOption};
 use assert_str::assert_str_eq;
 use url::Url;
 use veruna_data::SiteRepository;
 use veruna_kernel::sites::site_kit::SiteKitFactory;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
 
 
-fn main() {
+fn test() {
     let mut site_kit = SiteKitFactory::build(SiteRepository::new());
     let site_builder = site_kit.site_builder();
     let site = site_builder.build();
@@ -25,4 +38,18 @@ fn main() {
 
     let reader = site_kit.reader();
     let site = reader.read(site_id);
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("127.0.0.1", 20921))?
+        .run()
+        .await
 }
